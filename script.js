d92fc7bfefec255ff1402b10fb21f6cf1b91b286
shochmut <50213094+shochmut@users.noreply.github.com>
2023-03-19 19:12:46 -0600

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-console.log('Hello!');
+// initialize calculator value
+storedNum = '0';
+
+// initialize event handling
+const buttons = document.querySelector('.calculator')
+buttons.addEventListener('click', function() {
+  buttonClick(event.target.value)
+  reRenderDisplay()
+})
+
+const buttonClick = (value) => {
+  if (isNaN(value)) {
+    handleSymbol(value)
+  }
+  else {
+    handleNumber(value)
+  }
+}
+
+const handleSymbol = (symbol) => {
+  switch (symbol) {
+    case 'reset':
+      storedNum = '0'
+    break;
+    case 'backspace':
+      storedNum = storedNum.slice(0,-1)
+    break;
+    case 'divide':
+      divide(storedNum)
+    break;
+    case 'multiply':
+    
+    break;
+    case 'subtract':
+
+    break;
+    case 'add':
+
+    break;
+    case 'equals':
+
+    break;
+  }
+}
+
+const handleNumber = (number) => {
+  if (storedNum === '0') {
+    storedNum = number
+  }
+  else {
+    storedNum += number
+  }
+}
+
+const reRenderDisplay = () => {
+  document.querySelector('.display').innerText = storedNum
+}
+
+//mathematical operation functions
+const divide = (firstNum) => {
+  
+
+}
+
+
+
